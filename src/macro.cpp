9cbb763c8edada405b9c94e252bca5d7ef67988b
LukeLinux <wulinshuxue@qq.com>
2017-07-30 21:20:08 +0800

    remove valus in macro.cpp and update param.json

--- src/macro.cpp
+++ src/macro.cpp
@@ -2,15 +2,15 @@
 #include <QDebug>
 bool  ExternV_IS_DEBUG_MODE=false;
 
-uint ExternV_TargetCount= 40000;
+uint ExternV_TargetCount; //see param.json
 
 uint ExternV_SECONDS_CHECK_TARGET_COUNT= 20; //Output target count periodically
 uint ExternV_Milliseconds_FetchData = 4000;
 
-uint ExternV_SOGX10_LOWER_THRESH=30; //Only simulate targets with SOGX10 no less than this
-uint ExternV_SOGX10_UPPER_THRESH=800; //Only simulate targets with SOGX10 no larger than this
+uint ExternV_SOGX10_LOWER_THRESH; //Only simulate targets with SOGX10 no less than this.see param.json
+uint ExternV_SOGX10_UPPER_THRESH; //Only simulate targets with SOGX10 no larger than this. see param.json
 
-double ExternV_LATITUDE_LOWER_THRESH_DEGREE=30.0;
-double ExternV_LATITUDE_UPPER_THRESH_DEGREE=40.0;
-double ExternV_LONGITUDE_LOWER_THRESH_DEGREE=110.0;
-double ExternV_LONGITUDE_UPPER_THRESH_DEGREE=120.0;
+double ExternV_LATITUDE_LOWER_THRESH_DEGREE; //see param.json
+double ExternV_LATITUDE_UPPER_THRESH_DEGREE;//see param.json
+double ExternV_LONGITUDE_LOWER_THRESH_DEGREE;//see param.json
+double ExternV_LONGITUDE_UPPER_THRESH_DEGREE;//see param.json
